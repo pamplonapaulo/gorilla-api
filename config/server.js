409244d4657cb7bda5806659dfcdd57b97ee6674
PAULO PAMPLONA <paulopamplona@PAULOs-MacBook-Pro.local>
2021-09-22 16:04:27 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2b683df259c096a120e0ba65e9c64a25'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4be4c78a-bcfe-49a6-9679-b4f9737e7a3c'
+};
